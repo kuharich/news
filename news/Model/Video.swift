84144daf974b16977287085ce723e13bdeb7b795
Mark Kuharich <kuharich@hotmail.com>
2018-07-25 10:21:51 -0700

    Added Video struct

---
+++ news/Model/Video.swift
@@ -0,0 +1,17 @@
+//
+//  Video.swift
+//  news
+//
+//  Created by Mark Kuharich on 7/25/18.
+//  Copyright © 2018 Tero Jankko. All rights reserved.
+//
+
+import Foundation
+
+struct Video {
+    var id: String
+    var headline: String
+    var publishedDate: Date
+    var tease: String
+    var summary: String
+}
